585e694ebd4552cbb7c09a5954c46dcf2cd8c0eb
julia <zhydanovajulia@gmail.com>
2011-10-24 14:44:00 +0300

    changes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -21,6 +21,18 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content =>
+                          "Ruby on Rails Tutorial Sample App | Help")
+    end
 
     it "should have the right title" do
       get 'contact'
