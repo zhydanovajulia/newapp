c8940601fda7351633368de7401f59d79d80c481
julia <zhydanovajulia@gmail.com>
2011-10-24 03:26:36 +0300

    statis pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Newapp::Application.routes.draw do
+get "pages/home"
+get "pages/contact"
+get "pages/about"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
