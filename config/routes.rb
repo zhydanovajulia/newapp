1e892779b565a5b8ec253db614b071ff313354d7
julia <zhydanovajulia@gmail.com>
2011-10-26 03:52:22 +0300

    Done with sign IN

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Newapp::Application.routes.draw do
-resources :users
   
+
+
+resources :users
+resources :sessions, :only => [:new, :create, :destroy]
+ 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/signup',  :to => 'users#new'
-  
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
+  #match '/sessions', :to => 'sessions#create'
   root :to => 'pages#home'
  
   # The priority is based upon order of creation:
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,35 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+
+	describe "GET 'new'" do
+  
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field" do
+		get :new
+        response.should have_selector("input[name='user[email]'][type='text']")
+	end
+
+    it "should have a password field" do
+        get :new
+        response.should have_selector("input[name='user[password]'][type='password']")
+     end
+
+    it "should have a password confirmation field" do
+		get :new
+        response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+	end
+	
+  end
+
+
+
+
+
 describe "POST 'create'" do
 
     describe "failure" do
@@ -35,6 +64,11 @@ describe "POST 'create'" do
       before(:each) do
         @attr = { :name => "New User", :email => "user@example.com",
                   :password => "foobar", :password_confirmation => "foobar" }
+      end
+
+	it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
       end
 
       it "should create a user" do
